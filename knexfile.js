ed6b478c69ff9c104468445b80c025fb4b42d2c8
Travis <tlgreg86@gmail.com>
2017-08-18 11:30:17 -0600

    add test environment

--- knexfile.js
+++ knexfile.js
@@ -21,22 +21,18 @@ module.exports = {
       directory: './db/migrations'
     },
     useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/test',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/test/seeds'
+    }
   }
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
 };
