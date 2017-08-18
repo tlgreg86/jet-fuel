05c0ec9170def7e6dd1e37464fb0c70e5fafc618
Travis <tlgreg86@gmail.com>
2017-08-18 10:25:26 -0600

    Add heroku information to server.js

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,15 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.eng.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 
   // staging: {
@@ -30,20 +39,4 @@ module.exports = {
   //   }
   // },
   //
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
 };
